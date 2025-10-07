d88708fd6e7c5fc3287cf0a696b056f3d2fe346e
Antonio Pelayo <158385881+antoniopelayo1902@users.noreply.github.com>
2025-10-06 20:59:53 -0600

    Delete dist directory
    se me olvido en el gitignore

--- dist/src/app/controllers/users.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUsers = getUsers;
-function getUsers(req, res) {
-    res.send({ data: [] });
-}
--- dist/src/app/routes/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const users_1 = require("../controllers/users");
-const auth_1 = require("../middelwares/auth");
-const router = (0, express_1.Router)();
-router.use(auth_1.authMiddelware);
-router.get('', auth_1.authMiddelware, users_1.getUsers);
-exports.default = router;
